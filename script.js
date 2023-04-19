b2d81632d8bc37bb5b397a3e09c398908077167b
SHUBHAM VERMA <66116917+shubhamv1@users.noreply.github.com>
2023-04-19 10:30:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
-/* Edit this file */
-const player = document.querySelector('.player');
-const video = player.querySelector('.viewer');
-const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress__filled');
-const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player__slider');
+// Get DOM elements
+const video = document.querySelector('.player__video viewer');
+const progress = document.querySelector('.progress__filled');
+const playButton = document.querySelector('player__button toggle');
+const volumeSlider = document.querySelector('.player__slider[name="volume"]');
+const playbackSpeedSlider = document.querySelector('.player__slider[name="playbackRate"]');
+const skipButtons = document.querySelectorAll('[data-skip]');
+const skip10sButton = document.querySelector('.player__button[name="10"]');
+const skip25sButton = document.querySelector('.player__button[name="25"]');
+
+// Play or pause video
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+    playButton.textContent = '❚ ❚';
+  } else {
+    video.pause();
+    playButton.textContent = '►';
+  }
+}
+
+// Update progress bar
+function updateProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progress.style.flexBasis = `${percent}%`;
+}
+
+// Update volume
+function updateVolume() {
+  video.volume = volumeSlider.value;
+}
+
+// Update playback speed
+function updatePlaybackSpeed() {
+  video.playbackRate = playbackSpeedSlider.value;
+}
+
+// Skip forward or backward by specified seconds
+function skip(seconds) {
+  video.currentTime += seconds;
+}
+
+// Add event listeners
+video.addEventListener('click', togglePlay);
+playButton.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+volumeSlider.addEventListener('input', updateVolume);
+playbackSpeedSlider.addEventListener('input', updatePlaybackSpeed);
+skip10sButton.addEventListener('click', () => skip(-10));
+skip25sButton.addEventListener('click', () => skip(25));
+skipButtons.forEach(button => {
+  button.addEventListener('click', () => skip(parseFloat(button.dataset.skip)));
+});
